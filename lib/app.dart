07deb235f56c08b2f4507add5ae4b5fff11ee893
developer <developer@developer>
2018-11-14 16:13:55 +0200

    init project
    add: custom circle progress bar

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'component/main.dart';
+
+void main() => runApp(MyApp());
+
+
---
+++ lib/component/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'custom.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Container(
+              width: 300,
+              height: 300,
+              child: CustomPaint(
+                foregroundPainter: MyPainter(
+                    lineColor: Colors.yellow,
+                    completeColor: Colors.red,
+                    width: 30.0,
+                    completePercent: 45.0),
+                child: new Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: new RaisedButton(
+                    color: Colors.purple,
+                    splashColor: Colors.blueAccent,
+                    shape: new CircleBorder(),
+                    child: new Text("Click"),
+                    onPressed: () {},
+                  ),
+                ),
+              ))),
+    );
+  }
+}
